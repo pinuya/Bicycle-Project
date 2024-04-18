a8e825929cff1ec84f44bd2b181d750d038dfa5e
Tifany <tifsnunes@gmail.com>
2024-04-18 20:43:52 -0300

    js: Parameters URL

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// PARAMETROS URL
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
